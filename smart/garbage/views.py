226780e47c1d86e5041c6dbd73e88d5354cd7195
epngadem <162624680+epngadem@users.noreply.github.com>
2024-10-14 15:36:22 -0300

    Ajout des fichiers de vues, URL et template pour l'affichage des poubelles

--- smart/garbage/views.py
+++ smart/garbage/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Bin
 
-# Create your views here.
+def bin_list(request):
+    # Récupérer toutes les poubelles avec leurs données collectées
+    bins = Bin.objects.prefetch_related('data_collections').all()
+    return render(request, 'garbage/bin_list.html', {'bins': bins})
