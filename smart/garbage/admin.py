9593492bc10d3a9d7c5eb07c65beb4bc276f3f07
epngadem <162624680+epngadem@users.noreply.github.com>
2024-10-29 22:41:08 -0300

    Mise à jour pour inclure tous les fichiers essentiels

--- smart/garbage/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Bin, DataCollection
-
-
-admin.site.register(Bin)
-admin.site.register(DataCollection)
-
--- smart/garbage/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GarbageConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'garbage'
