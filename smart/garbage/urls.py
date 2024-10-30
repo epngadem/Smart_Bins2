7f4a44dd7f7545edc908412a6f8008925b48650d
epngadem <162624680+epngadem@users.noreply.github.com>
2024-10-29 22:54:02 -0300

    Ajout forcé des fichiers ignorés

---
+++ smart/garbage/urls.py
@@ -0,0 +1,20 @@
+# garbage/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+from .views import BinViewSet, DataCollectionViewSet
+
+# Création du routeur DRF pour les API
+router = DefaultRouter()
+router.register(r'bins', BinViewSet)
+router.register(r'data', DataCollectionViewSet)
+
+# Définition des URLs
+urlpatterns = [
+    path('', views.bin_list, name='bin_list'),  # Page principale avec la liste des poubelles
+    path('add/', views.add_bin, name='add_bin'),  # Ajouter une poubelle
+    path('data/add/', views.add_data, name='add_data'),  # Ajouter des données
+    path('home/', views.home, name='home'),  # Page d'accueil
+    path('moyenne-temperature/', views.moyenne_temperature, name='moyenne_temperature'),  # Moyenne des températures
+    path('api/', include(router.urls)),  # Inclusion des URLs du routeur API
+]
